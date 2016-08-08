8d103531076a241137ec3a160926b28f5cfa0721
Devina.Kumar.18@dartmouth.edu <devinakumar@devinas-air-2.wireless.dartmouth.edu>
2016-08-08 17:47:41 -0400

    adding server

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,8 @@ export const ActionTypes = {
   DELETE_POST: 'DELETE_POST',
 };
 
-const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
+// const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
+const ROOT_URL = 'http://localhost:9090/api';
 const API_KEY = '?key=devina_k';
 
 export function fetchPosts() {
